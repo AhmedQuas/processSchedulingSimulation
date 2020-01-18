e0aeacbf1c12a8e4a9b58f3dc9f0fb0bd00c41a5
AhmedQuas <ahmed.quas@gmail.com>
2020-01-18 21:57:30 +0100

    save_result() method in statistic module is implemented

--- statistic/statistic.py
+++ statistic/statistic.py
@@ -7,12 +7,25 @@ class AlgorithmStatistic:
         self.whole_stats = {}
 
     def save_results(self):
-        return "Not implemented method"
+        file = open(self.filename, 'w')
+        set_counter = 1
+        for stat_result in self.process_stats:
+            file.write('{}.\n-Average process waiting time: {}\n-Average process processing time: {}\n'
+                       .format(set_counter,
+                               stat_result['waiting_aver'],
+                               stat_result['processing_aver']))
+            set_counter += 1
+
+        file.write('\n\t+===============+\n\t| Total results |\n\t+===============+\n\n')
+        file.write('Average process waiting time: {}\nAverage process processing time: {}\n'.format(
+                    self.whole_stats['waiting_aver'],
+                    self.whole_stats['processing_aver']))
+        file.close()
 
     def generate_stats(self):
         self.get_queue_stats()
         self.get_all_stats()
-        # self.save_results()
+        self.save_results()
 
     def get_all_stats(self):
         n = len(self.process_stats)
@@ -20,7 +33,7 @@ class AlgorithmStatistic:
             sum_stat_type = 0
             for proc in self.process_stats:
                 sum_stat_type += proc[stat_type]
-            self.whole_stats[stat_type] = sum_stat_type/n
+            self.whole_stats[stat_type] = round(sum_stat_type/n, 2)
 
     def get_queue_stats(self):
         for process_set in self.finished_process:
@@ -35,7 +48,7 @@ class AlgorithmStatistic:
         for process in process_set:
             waiting_sum += process.get_waiting_time()
         average = waiting_sum/n
-        return average
+        return round(average, 2)
 
     def get_processing_stat(self, process_set):
         n = len(process_set)
@@ -43,4 +56,4 @@ class AlgorithmStatistic:
         for process in process_set:
             waiting_sum += process.get_processing_time()
         average = waiting_sum / n
-        return average
+        return round(average, 2)
